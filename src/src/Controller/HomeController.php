3167c42cc9569ac09219d3dbbdb3917998538bb5
Zeppe_PC <jakub.juszczyk7@gmail.com>
2020-09-19 22:05:22 +0100

    feat(deps): add orm

--- src/src/Controller/HomeController.php
+++ src/src/Controller/HomeController.php
@@ -1,8 +1,7 @@
 <?php
 declare(strict_types = 1);
 
-namespace App\Controller\Web\Controller;
-
+namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
